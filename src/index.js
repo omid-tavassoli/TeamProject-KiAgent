f8c75187ab904c84522d6a7b3a07037ba7e945dc
omid-tavassoli <omidddd105@gmail.com>
2025-10-31 18:13:46 +0100

    your change

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const element = <h1>Hello Iman</h1>;
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const element = <h1>Hello Iman</h1>
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(element)
